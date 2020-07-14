532686e171d3f82c4f87c27e6828ddd1a343dbb1
Paul Jerome <mr.paulthomasjerome@gmail.com>
2020-07-13 18:19:31 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import FullPanel from './components/containers/FullPanel.js';
+
+const App = () => {
+  return (
+    <div>
+      <FullPanel>
+
+      </FullPanel>
+      <FullPanel>
+        
+      </FullPanel>
+      <FullPanel>
+        
+      </FullPanel>
+    </div>
+
+  );
+}
+
+export default App;
